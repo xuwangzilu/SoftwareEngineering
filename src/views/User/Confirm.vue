52601fa826f9bf7a7714fb72e848c4276c91cc69
xuwangzilu <501661611@qq.com>
2022-01-07 22:37:37 +0800

    修改了个人信息
    修改了个人信息。

--- src/views/User/Confirm.vue
+++ src/views/User/Confirm.vue
@@ -1,22 +1,67 @@
 <template>
-<div id="name">
-	
+<div class="confirm">
+    <div class="title">
+        确认订单
+    </div>
+	<el-card class="card">
+        <el-form label-width="90px">
+            <el-form-item label="商品价格:">
+                ￥500
+            </el-form-item>
+            <el-form-item label="交易地址:">
+                <div v-if="sellerLocation.length">{{sellerLocation}}</div>
+                <el-input v-else v-model="location"></el-input>
+            </el-form-item>
+            <el-form-item label="地址说明:">
+                <div v-if="sellerLocationDescription.length">{{sellerLocationDescription}}</div>
+                <el-input v-else v-model="locationDescription" autosize type="textarea"></el-input>
+            </el-form-item>
+            <el-form-item v-if="sellerTimeDescription.length" label="时间限制:">
+                <el-input v-model="locationDescription" autosize type="textarea"></el-input>
+            </el-form-item>
+            <el-form-item label="交易日期:">
+                <el-date-picker type="date" placeholder="Pick a day"/>
+            </el-form-item>
+            <el-form-item label="交易时间:">
+                <el-time-picker format="HH:mm" placeholder="Arbitrary time"/>
+            </el-form-item>
+        </el-form>
+    </el-card>
 </div>
 </template>
 
-<script>
+<style scoped>
+.confirm{
+    width: 400px;
+    margin: 5px auto;
+}
+.title{
+    margin: 15px 0 10px;
+    font-size: 40px;
+    font-weight: 800;
+    color: #262626;
+    font-family: '楷体';
+}
+.card{
+    padding: 0 20px;
+}
+</style>
 
+<script>
 export default {
     components:{
         
     },
     data(){
         return{
-
+            sellerLocation: '111',
+            sellerLocationDescription: '1111',
+            sellerTimeDescription: '',
+            location: '',
+            locationDescription: '',
         }
     },
     methods:{
-
-    },
+    }
 }
 </script>
